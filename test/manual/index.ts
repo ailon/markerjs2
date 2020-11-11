1dfb4010dfb679807a3ab4a318102e970dd5e907
Alan Mendelevich <ailon@ailon.org>
2020-11-11 17:03:25 +0200

    add display mode selection to test page

--- test/manual/index.ts
+++ test/manual/index.ts
@@ -1,12 +1,16 @@
 import { MarkerArea } from '../../src';
+import { DisplayMode } from '../../src/core/Settings';
 
 export * from './../../src/index';
 
 export class Experiments {
   private markerArea1: MarkerArea;
+  private displayMode: DisplayMode = 'inline';
+
   public openMarkerArea(target: HTMLImageElement): void {
     this.markerArea1 = new MarkerArea(target);
     this.markerArea1.addRenderEventListener(this.renderResult);
+    this.markerArea1.settings.displayMode = this.displayMode;
     this.markerArea1.show();
   }
 
@@ -17,5 +21,9 @@ export class Experiments {
   public async render(resultTarget: HTMLImageElement): Promise<void> {
     resultTarget.src = await this.markerArea1.render();
     this.markerArea1.close();
+  }
+
+  public setDisplayMode(mode: DisplayMode): void {
+    this.displayMode = mode;
   }
 }
