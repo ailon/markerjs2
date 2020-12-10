c188a9f1af8bf5f6f623031c93a993f43c2a6cf1
Alan Mendelevich <ailon@ailon.org>
2020-12-10 11:24:59 +0200

    export all public classes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,45 @@
 export { MarkerArea } from './MarkerArea';
+export { MarkerAreaState } from './MarkerAreaState';
+
 export { Activator } from './core/Activator';
 export { SvgHelper } from './core/SvgHelper';
 export { Style, StyleClass } from './core/Style';
+export { Settings, ColorSet, DisplayMode } from './core/Settings';
+export { IStyleSettings } from './core/IStyleSettings';
+export { ITransformMatrix, TransformMatrix } from './core/TransformMatrix';
+
+export { ToolboxPanel } from './ui/ToolboxPanel';
+export { ArrowType, ArrowTypeChangeHandler, ArrowTypePanel } from './ui/toolbox-panels/ArrowTypePanel';
+export { ColorChangeHandler, ColorPickerPanel } from './ui/toolbox-panels/ColorPickerPanel';
+export { FontChangeHandler, FontFamilyPanel } from './ui/toolbox-panels/FontFamilyPanel';
+export { LineStylePanel, StyleChangeHandler } from './ui/toolbox-panels/LineStylePanel';
+export { LineWidthPanel, WidthChangeHandler } from './ui/toolbox-panels/LineWidthPanel';
+export { OpacityChangeHandler, OpacityPanel } from './ui/toolbox-panels/OpacityPanel';
 
 export { MarkerBase } from './core/MarkerBase';
+export { MarkerBaseState } from './core/MarkerBaseState';
+
+export { LinearMarkerBase } from './markers/LinearMarkerBase';
+export { LinearMarkerBaseState } from './markers/LinearMarkerBaseState';
+export { RectangleMarker } from './markers/RectangleMarker';
+export { RectangleMarkerState } from './markers/RectangleMarkerState';
+export { RectangularBoxMarkerBase } from './markers/RectangularBoxMarkerBase';
+export { RectangularBoxMarkerBaseState } from './markers/RectangularBoxMarkerBaseState';
+export { RectangularBoxMarkerGrips } from './markers/RectangularBoxMarkerGrips';
+export { ResizeGrip } from './markers/ResizeGrip';
+
+export { ArrowMarker } from './markers/arrow-marker/ArrowMarker';
+export { ArrowMarkerState } from './markers/arrow-marker/ArrowMarkerState';
+export { CalloutMarker } from './markers/callout-marker/CalloutMarker';
+export { CalloutMarkerState } from './markers/callout-marker/CalloutMarkerState';
+export { CoverMarker } from './markers/cover-marker/CoverMarker';
+export { EllipseMarker } from './markers/ellipse-marker/EllipseMarker';
+export { FrameMarker } from './markers/frame-marker/FrameMarker';
+export { FreehandMarker } from './markers/freehand-marker/FreehandMarker';
+export { FreehandMarkerState } from './markers/freehand-marker/FreehandMarkerState';
+export { HighlightMarker } from './markers/highlight-marker/HighlightMarker';
+export { LineMarker } from './markers/line-marker/LineMarker';
+export { LineMarkerState } from './markers/line-marker/LineMarkerState';
+export { MeasurementMarker } from './markers/measurement-marker/MeasurementMarker';
+export { TextMarker } from './markers/text-marker/TextMarker';
+export { TextMarkerState } from './markers/text-marker/TextMarkerState';
